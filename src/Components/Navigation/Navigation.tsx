10e03dd18772f1d59ca322064055849c278b30c6
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2026-01-15 13:04:43 +0100

    v9

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -10,28 +10,13 @@ export function Navigation({setIsOpen}:NavigationsProps){
 return(
 
 <nav className={style.navStyle}>
-    <ul>
-        <li className={style.liStyle}><a href="#">Home</a></li>
-        </ul>
-        <ul>
-        <li  className={style.liStyle}><a href="#">Sådan spiller du Yatzy</a></li>
-        </ul>
-        <ul>
-        <li  className={style.liStyle}><a href="#">PointSystemet</a></li>
-        </ul>
-        <ul>
-        <li className={style.liStyle}><a href="#">Regler</a></li>
-    </ul>
-
-    <ul><button className={style.yatzybuttonStyle} onClick={()=> setIsOpen(true)}>
-        <b>Yatzy</b></button></ul>
+   
     <ul><button className={style.pointbuttonStyle} onClick={()=> setIsOpen(true)}>
         <b>PointSystem</b></button></ul>
     <ul>
     <button className={style.buttonStyle} onClick={()=> setIsOpen(true)}>
-        <b>Regler</b></button></ul>
-        <ul><button className={style.loginbuttonStyle} onClick={()=> setIsOpen(true)}>
-        <b>LogIn</b></button></ul>
+        <b>Spilleregler</b></button></ul>
+       
 </nav>
 )
 }
